b2afc434892aad571f48ddcc58cc0e063d6293ac
Gandjurov <gandjurov@abv.bg>
2019-03-05 21:32:29 +0200

    Add structure

---
+++ 02.Web Server - HTTP Protocol/WebServerHTTPProtocol/SoftUniHttpSever/HttpServer.cs
@@ -0,0 +1,29 @@
+﻿using SoftUniHttpSever.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SoftUniHttpSever
+{
+    public class HttpServer : IHttpServer
+    {
+        private TcpListener tcpListener;
+
+        public HttpServer()
+        {
+            this.tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 02.Web Server - HTTP Protocol/WebServerHTTPProtocol/SoftUniHttpSever/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using SoftUniHttpSever.Contracts;
+using System;
+
+namespace SoftUniHttpSever
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            IHttpServer server = new HttpServer();
+
+            server.Start();
+        }
+    }
+}
