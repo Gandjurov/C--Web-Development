3734dae528b5cddaafd68fdb64496010327e6f54
Gandjurov <gandjurov@abv.bg>
2019-05-01 14:33:37 +0300

    delete unnecessary code

--- 02. WebServerHTTPProtocol/SoftUniHttpSever/StartUp.cs
+++ 02. WebServerHTTPProtocol/SoftUniHttpSever/StartUp.cs
@@ -8,17 +8,9 @@ namespace SoftUniHttpSever
     {
         public static void Main()
         {
-            //IHttpServer server = new HttpServer();
+            IHttpServer server = new HttpServer();
 
-            //server.Start();
-
-            var inputString = WebUtility.UrlDecode(Console.ReadLine());
-            var uri = new Uri(inputString);
-            var uriScheme = uri.Scheme;
-            var uriServer = uri.Segments;
-
-            Console.WriteLine($"[Protocol] = {uriScheme}");
-            Console.WriteLine($"[Protocol] = {uriServer}");
+            server.Start();
 
         }
     }
