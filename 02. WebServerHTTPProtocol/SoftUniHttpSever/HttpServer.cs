75d20f396a925ea1538078b226612816fe902741
Gandjurov <gandjurov@abv.bg>
2019-05-08 12:10:13 +0300

    Add Asynchronous about ProcessClient

--- 02. WebServerHTTPProtocol/SoftUniHttpSever/HttpServer.cs
+++ 02. WebServerHTTPProtocol/SoftUniHttpSever/HttpServer.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace SoftUniHttpSever
 {
@@ -28,28 +29,35 @@ namespace SoftUniHttpSever
             while (isWorking)
             {
                 var client = this.tcpListener.AcceptTcpClient();
-                var buffer = new byte[10240];
-                var stream = client.GetStream();
-                var readLength = stream.Read(buffer, 0, buffer.Length);
-                var requestText = Encoding.UTF8.GetString(buffer, 0, readLength);
-                Console.WriteLine(new string('=', 60));
-                Console.WriteLine(requestText);
-                //Thread.Sleep(10000); We'll get back here, when testing asynchronic programming
-
-                var responseText = File.ReadAllText("index.html");
-
-                var responseBytes = Encoding.UTF8.GetBytes("HTTP/1.0 200 OK" + Environment.NewLine +
-                                                           "Location: https://softuni.bg" + Environment.NewLine + Environment.NewLine +
-                                                           "Content-Type: text/html" + Environment.NewLine + 
-                                                           "Content-Disposition: attachment; filename=index.html" + Environment.NewLine +
-                                                           "Content-Length: " + responseText.Length +
-                                                           Environment.NewLine + Environment.NewLine + 
-                                                           responseText);
-                stream.Write(responseBytes);
-                //Thread.Sleep(10000);
-                stream.Write(Encoding.UTF8.GetBytes("<h1>@</h1>"));
+                Task.Run(() => ProcessClient(client));
             }
 
+        }
+
+        private static async void ProcessClient(TcpClient client)
+        {
+            var buffer = new byte[10240];
+            var stream = client.GetStream();
+            Console.WriteLine($"{client.Client.RemoteEndPoint} => {Thread.CurrentThread.ManagedThreadId}");
+            var readLength = await stream.ReadAsync(buffer, 0, buffer.Length);
+            Console.WriteLine($"{client.Client.RemoteEndPoint} => {Thread.CurrentThread.ManagedThreadId}");
+            var requestText = Encoding.UTF8.GetString(buffer, 0, readLength);
+            //Console.WriteLine(new string('=', 60));
+            //Console.WriteLine(requestText);
+            await Task.Run(() => Thread.Sleep(10000));
+            var responseText = File.ReadAllText("index.html");
+
+            var responseBytes = Encoding.UTF8.GetBytes("HTTP/1.0 200 OK" + Environment.NewLine +
+                                                       "Location: https://softuni.bg" + Environment.NewLine + Environment.NewLine +
+                                                       "Content-Type: text/html" + Environment.NewLine +
+                                                       "Content-Disposition: attachment; filename=index.html" + Environment.NewLine +
+                                                       "Content-Length: " + responseText.Length +
+                                                       Environment.NewLine + Environment.NewLine +
+                                                       responseText);
+            stream.Write(responseBytes);
+            Console.WriteLine($"{client.Client.RemoteEndPoint} => {Thread.CurrentThread.ManagedThreadId}");
+            //Thread.Sleep(10000);
+            await stream.WriteAsync(Encoding.UTF8.GetBytes("<h1>@</h1>"));
         }
 
         public void Stop()
